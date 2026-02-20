5cf284eb54df6edc56d74ab26a3ea2276d4d6e7f
Дилян Палаузов <git-dpa@aegee.org>
2026-02-20 10:50:30 +0200

    TimeoutException: utilize java.io.Serial annotation

--- src/main/java/be/bapha/openhab/binding/voltronic/internal/TimeoutException.java
+++ src/main/java/be/bapha/openhab/binding/voltronic/internal/TimeoutException.java
@@ -1,11 +1,14 @@
 package be.bapha.openhab.binding.voltronic.internal;
 
+import java.io.Serial;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
 
 /** Fired for disconnected devices **/
 @NonNullByDefault
 class TimeoutException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public TimeoutException() {
